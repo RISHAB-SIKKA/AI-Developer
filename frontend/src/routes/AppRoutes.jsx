e88e575e3dac85f59e7087819050c7388eb78082
RISHAB-SIKKA <sikka.rishab@gmail.com>
2025-04-03 18:25:53 +0530

    Done for the day. Start from 03:10

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import Project from '../pages/Project';
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,8 @@ const AppRoutes = () => {
         <Route path="/" element={ <Home /> } />
         <Route path="/login" element={ <Login /> } />
         <Route path="/register" element={ <Register /> } />
+        <Route path="/project" element={ <Project /> } />
+
 
     </Routes>
     </BrowserRouter>
